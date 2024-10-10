a3ea5c4b2eba486cf38d4e1d877d8f2e7d165bfb
Afeefahafeefah <afeefah.cs23@bmsce.ac.in>
2024-10-10 13:35:18 +0530

    Add files via upload

---
+++ JAVA LAB1/Quadratic.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.lang.Math;
+class Quadratic
+ {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("AFEEFAH 1BM23CS015");
+        System.out.print("Enter coefficient a: ");
+        double a = scanner.nextDouble();
+        
+        System.out.print("Enter coefficient b: ");
+        double b = scanner.nextDouble();
+        
+        System.out.print("Enter coefficient c: ");
+        double c = scanner.nextDouble();
+        
+        double d = b * b - 4 * a * c;
+        
+        if (d > 0) {
+            double r1 = (-b + Math.sqrt(d)) / (2 * a);
+            double r2 = (-b - Math.sqrt(d)) / (2 * a);
+            System.out.println("The roots are real and different:");
+            System.out.println("Root 1: " + r1);
+            System.out.println("Root 2: " + r2);
+        } else if (d == 0) {
+            double r = -b / (2 * a);
+            System.out.println("The roots are real and the same:");
+            System.out.println("Root: " + r);
+        } else {
+            System.out.println("The roots are complex:");
+            double realPart = -b / (2 * a);
+            double imaginaryPart = Math.sqrt(-d) / (2 * a);
+            System.out.println("Root 1: " + realPart + " + " + imaginaryPart + "i");
+            System.out.println("Root 2: " + realPart + " - " + imaginaryPart + "i");
+        }
+        
+        scanner.close();
+        
+    }
+}
